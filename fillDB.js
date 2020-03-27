b6bd1b5f56256522b2f911dae1b93276bc13fb97
Cédric <github@hidemyhome.ovh>
2020-03-27 15:33:55 +0100

    Ajout d'un post dans fillDB pour prévénir les utilisateurs que la BDD est reset quotidiennement + suppression du fichier .json inutilisé

--- fillDB.js
+++ fillDB.js
@@ -2,8 +2,18 @@ var casual = require("casual");
 
 module.exports = () => {
   const data = { posts: [] };
+  
+  // Create a description for new user
+  data.posts.push({
+	  id: 0,
+      title: "Testez-moi",
+      content: "N'hésitez pas à supprimer ou créer des posts. La base de données est vidée tous les soirs ;-)",
+      author: "Le Webmaster"
+  })
+  
+  
   // Create 1000 users
-  for (let i = 0; i < 25; i++) {
+  for (let i = 1; i < 25; i++) {
     data.posts.push({
       id: i,
       title: casual.title,
